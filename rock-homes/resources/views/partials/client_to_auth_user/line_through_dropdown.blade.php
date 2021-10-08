e40ec81fc3db4a835eac0a50624853de6f0fa64a
James <jamesonyemi@gmail.com>
2021-10-08 15:37:45 +0000

    formated text to readibility

--- rock-homes/resources/views/partials/client_to_auth_user/line_through_dropdown.blade.php
+++ rock-homes/resources/views/partials/client_to_auth_user/line_through_dropdown.blade.php
@@ -1,20 +1,20 @@
 <select 
     id="clientid" 
     name="clientid" 
-    class="form-control selectpicker" 
+    class="form-control selectpicker show-tick" 
     data-live-search="true" required >
     <option value="" disabled>---select---</option>
         @foreach ($clients as $key => $client)
-        <?php $client_fullname = ucwords( ($client->client_name)) ?>
+        <?php $client_full_name = ucwords( ($client->client_name)) ?>
         <option 
         class="text-capitalize"
         value="{{ $client->id}}"
         {{ ( in_array($client->id, @$project))  ? 'disabled' : '' }}
         data-content="{{  
-        ( in_array($client->id, @$project))  ? '<del>'.$client_fullname.'</del>' 
+        ( in_array($client->id, @$project))  ? '<span class="badge badge-danger"><del>'.$client_full_name .'</del></span>' 
                                 : $client->client_name }}   " >
                                 
-        {{ !empty($client_fullname) ? $client_fullname : "" }}
+        {{ !empty($client_full_name) ? $client_full_name : "" }}
     </option>
     @endforeach
 </select>
