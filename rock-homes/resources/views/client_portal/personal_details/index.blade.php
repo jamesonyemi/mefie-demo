bc50b7ae3f30a0cce37274941f4a910c56b511d6
James <jamesonyemi@gmail.com>
2021-10-23 17:54:32 +0000

    added text-capitalized, to change the first letter of every to uppercase

--- rock-homes/resources/views/client_portal/personal_details/index.blade.php
+++ rock-homes/resources/views/client_portal/personal_details/index.blade.php
@@ -12,13 +12,13 @@
                         <div class="form-group col-md-1"> </div>
                         <div class="form-group col-md-4">
                             <label for="first_name">Full Name</label>
-                        <input type="text" class="form-control list-group-item" name="first_name" id="first_name" 
+                        <input type="text" class="form-control list-group-item text-capitalize" name="first_name" id="first_name" 
                             value="{!! $individual_clients->full_name !!}" disabled>        
                         </div>
                         <div class="form-group col-md-1"> </div>
                         <div class="form-group col-md-4">
                             <label for="middle_name">Client Primary Phone</label>
-                        <input type="text" class="form-control list-group-item" name="primary_contact" id="primary_contact"
+                        <input type="text" class="form-control list-group-item text-capitalize" name="primary_contact" id="primary_contact"
                             value="{!! $individual_clients->phone1 !!}" disabled >        
                         </div>
                     </div>
@@ -26,36 +26,36 @@
                         <div class="form-group col-md-1"> </div>
                         <div class="form-group col-md-4">
                             <label for="email">Email</label>
-                        <input type="email" class="form-control list-group-item" name="email" id="email" value="{!! $individual_clients->email !!}" disabled>  
+                        <input type="email" class="form-control list-group-item text-capitalize" name="email" id="email" value="{!! $individual_clients->email !!}" disabled>  
                         </div>
                         <div class="form-group col-md-1"> </div>
                         <div class="form-group col-md-4" >
                             <label for="nat">Nationality</label>
-                            <input type="text" class="form-control list-group-item" name="Nationality" value="{!! $individual_clients->nationality !!}" disabled>  
+                            <input type="text" class="form-control list-group-item text-capitalize" name="Nationality" value="{!! $individual_clients->nationality !!}" disabled>  
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-1"> </div>
                         <div class="form-group col-md-4">
                             <label for="dob">Date Of Birth</label>
-                            <input type="text" class="form-control list-group-item" name="dob" value="{!! date("l, jS F, Y", strtotime($individual_clients->dob))  !!}" disabled>
+                            <input type="text" class="form-control list-group-item text-capitalize" name="dob" value="{!! date("l, jS F, Y", strtotime($individual_clients->dob))  !!}" disabled>
                         </div>
                         <div class="form-group col-md-1"> </div>
                         <div class="form-group col-md-4" >
                             <label for="kin">Next of Kin</label>
-                            <input type="text" class="form-control list-group-item" name="nkin" value="{!! $individual_clients->nok !!}" disabled>  
+                            <input type="text" class="form-control list-group-item text-capitalize" name="nkin" value="{!! $individual_clients->nok !!}" disabled>  
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-1"> </div>
                         <div class="form-group col-md-4">
                             <label for="rel-kin">Relationship to Next of Kin</label>
-                            <input type="text" class="form-control list-group-item" name="rel-kin" value="{!! ucfirst($individual_clients->relationship) !!}" disabled>
+                            <input type="text" class="form-control list-group-item text-capitalize" name="rel-kin" value="{!! ucfirst($individual_clients->relationship) !!}" disabled>
                         </div>
                         <div class="form-group col-md-1"> </div>
                         <div class="form-group col-md-4" >
                             <label for="kin">Phone Number of Next of Kin</label>
-                            <input type="text" class="form-control list-group-item" name="nkin" value="{!! $individual_clients->nokphone !!}" disabled>  
+                            <input type="text" class="form-control list-group-item text-capitalize" name="nkin" value="{!! $individual_clients->nokphone !!}" disabled>  
                         </div>
                     </div><br><br>
                 </form>
