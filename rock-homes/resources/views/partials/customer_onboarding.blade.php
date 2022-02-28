0f5d5a9525d7d0e0d2f17c92ade52b06376a6ebb
James <jamesonyemi@gmail.com>
2022-02-28 11:43:48 +0000

    Customr OnBoarding Page Customization:
    - add 'badeg-danger' to enforce a prompt call-to-action from newly register customer to complete the process -- activating their account on time.
    
    - show customer the expiration date for account activation

--- rock-homes/resources/views/partials/customer_onboarding.blade.php
+++ rock-homes/resources/views/partials/customer_onboarding.blade.php
@@ -12,7 +12,11 @@
                       <h2 class="my-5 ml-5">{{ $success }}</h2>
                     </div>
                     <h4>Please we've sent you an email to activate your account on {{ config("app.name")  }}</h4>
-                    <p class="">The link would expire soon, please activate your account</p>
+                    <p class="badge-danger rounded rounded-pill text-white">
+                        <i class='bx bxs-hand-right bx-tada mr-2 '></i>
+                        The activation link will expire 
+                        {{ now()->addMonths(1)->toDateString() }}
+                    </p>
                 </div>
             </div>
         </div>
