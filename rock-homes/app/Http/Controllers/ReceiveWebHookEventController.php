9c30bb6670c6b867fba6739455f6a2aa9ce2f53e
James <jamesonyemi@gmail.com>
2022-03-02 05:51:00 +0000

    verify transaction

--- rock-homes/app/Http/Controllers/ReceiveWebHookEventController.php
+++ rock-homes/app/Http/Controllers/ReceiveWebHookEventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Traits\ReceiveWebHookEventTrait;
 use App\Traits\VerifyPayStackPaymentTrait as VerifyTransaction;
 
@@ -22,7 +23,7 @@ class ReceiveWebHookEventController extends Controller
         // Do something with $response
 
         if($response->data->status == "success") {
-            \DB::table('paystack_cust')->insertGetId([
+            DB::table('paystack_cust')->insertGetId([
                 'plan_code'     =>  $response->data->plan_object->plan_code,
                 'cust_email'    =>  $response->data->customer->email,
                 'cust_code'     =>  $response->data->customer->customer_code,
@@ -31,7 +32,7 @@ class ReceiveWebHookEventController extends Controller
             abort(302);
         }
         
-        http_response_code(200); // PHP 5.4 or greater
+        http_response_code(200);
 
     }
 }
